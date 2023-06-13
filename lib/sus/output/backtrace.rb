fa3f396627baf19a29c7aad65fe698899ca8f645
Joel Moss <joel@developwithstyle.com>
2023-06-13 22:48:37 +0100

    Verbose backtraces

--- lib/sus/output/backtrace.rb
+++ lib/sus/output/backtrace.rb
@@ -4,55 +4,55 @@
 # Copyright, 2022, by Samuel Williams.
 
 module Sus
-	module Output
-		# Print out a backtrace relevant to the given test identity if provided.
-		class Backtrace
-			def self.first(identity = nil)
-				# This implementation could be a little more efficient.
-				self.new(caller_locations(1), identity&.path, 1)
-			end
-			
-			def self.for(exception, identity = nil)
-				# I've disabled the root filter here, because partial backtraces are not very useful.
-				# We might want to do something to improve presentation of the backtrace based on the root instead.
-				self.new(exception.backtrace_locations) # , identity&.path)
-			end
-			
-			def initialize(stack, root = nil, limit = nil)
-				@stack = stack
-				@root = root
-				@limit = limit
-			end
-			
-			def filter(root = @root)
-				if @root
-					stack = @stack.select do |frame|
-						frame.path.start_with?(@root)
-					end
-				else
-					stack = @stack
-				end
-				
-				if @limit
-					stack = stack.take(@limit)
-				end
-				
-				return stack
-			end
-			
-			def print(output)
-				if @limit == 1
-					filter.each do |frame|
-						output.write " ", :path, frame.path, :line, ":", frame.lineno
-					end
-				else
-					output.indented do
-						filter.each do |frame|
-							output.puts :indent, :path, frame.path, :line, ":", frame.lineno, :reset, " ", frame.label
-						end
-					end
-				end
-			end
-		end
-	end
+  module Output
+    # Print out a backtrace relevant to the given test identity if provided.
+    class Backtrace
+      def self.first(identity = nil)
+        # This implementation could be a little more efficient.
+        new(caller_locations(1), identity&.path, 1)
+      end
+
+      def self.for(exception, _identity = nil, verbose = false)
+        # I've disabled the root filter here, because partial backtraces are not very useful.
+        # We might want to do something to improve presentation of the backtrace based on the root instead.
+        new(exception.backtrace_locations, verbose ? nil : Pathname.pwd.to_s)
+      end
+
+      def initialize(stack, root = nil, limit = nil)
+        @stack = stack
+        @root = root
+        @limit = limit
+      end
+
+      def filter(_root = @root)
+        stack = if @root
+                  @stack.select do |frame|
+                    !frame.path.start_with?('/') || frame.path.start_with?(@root)
+                  end
+                else
+                  @stack
+                end
+
+        stack = stack.take(@limit) if @limit
+
+        stack
+      end
+
+      def print(output)
+        if @limit == 1
+          filter.each do |frame|
+            path = @root ? "./#{frame.path.delete_prefix("#{@root}/")}" : frame.path
+            output.write ' ', :path, path, :line, ':', frame.lineno
+          end
+        else
+          output.indented do
+            filter.each do |frame|
+              path = @root ? "./#{frame.path.delete_prefix("#{@root}/")}" : frame.path
+              output.puts :indent, :path, path, :line, ':', frame.lineno, :reset, ' ', frame.label
+            end
+          end
+        end
+      end
+    end
+  end
 end
